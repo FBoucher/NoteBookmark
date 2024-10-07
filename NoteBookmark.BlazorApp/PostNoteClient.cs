7358837a0033113ea214a575c3b7580cddc36121
Frank Boucher <fboucher@outlook.com>
2024-10-07 09:11:42 -0400

    fix url

--- NoteBookmark.BlazorApp/PostNoteClient.cs
+++ NoteBookmark.BlazorApp/PostNoteClient.cs
@@ -8,7 +8,7 @@ public class PostNoteClient(HttpClient httpClient)
 
     public async Task<List<Post>> GetUnreadPosts()
     {
-        var posts = await httpClient.GetFromJsonAsync<List<Post>>("api/GetUnreadPosts");
+        var posts = await httpClient.GetFromJsonAsync<List<Post>>("api/posts");
         return posts ?? new List<Post>();
     }
 }
