85f2dab0475b75f24b94eb2f28d724a4cb02a1bf
Frank Boucher <fboucher@outlook.com>
2024-10-09 20:27:43 -0400

    test

--- NoteBookmark.AppHost/Program.cs
+++ NoteBookmark.AppHost/Program.cs
@@ -4,7 +4,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<NoteBookmark_Api>("api");
 
-builder.AddProject<NoteBookmark_BlazorApp>("blazorApp")
+builder.AddProject<NoteBookmark_BlazorApp>("blazor-app")
     .WithReference(api);    
 
 builder.Build().Run();
